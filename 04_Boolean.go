7593c79fbe47c1cca81ff553b4385518eb9fbd78
ファン・デヨン <77083563+KRHDY@users.noreply.github.com>
2021-04-23 19:53:17 +0900

    Update 04_Boolean.go

--- 04_Boolean.go
+++ 04_Boolean.go
@@ -9,16 +9,15 @@ func main()  {
 	// ******************************
 
 	fmt.Println(true && true);
-	// 참이고 참이면 참
+		// 참이고 참이면 참
 	fmt.Println(true && false);
-	// 참이고 거짓이면 거짓
+		// 참이고 거짓이면 거짓
 	fmt.Println(true || true);
-	// 참이거나 참이면 참
+		// 참이거나 참이면 참
 	fmt.Println(true || false);
-	// 참이거나 거짓이면 참
+		// 참이거나 거짓이면 참
 	fmt.Println(!true);
-	// 참의 거짓이면 거짓
+		// 참의 거짓이면 거짓
 	fmt.Println(!!false);
-	// 거짓의 거짓의 거짓 거짓
-
-	;}
+		// 거짓의 거짓의 거짓 거짓
+		}
