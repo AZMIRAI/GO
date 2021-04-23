9105af320e94e31e4b8de43c0ec824b0ad7a290e
ファン・デヨン <77083563+KRHDY@users.noreply.github.com>
2021-04-23 19:43:24 +0900

    Add files via upload
    section01 example

---
+++ 05_Section01_Example.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println(" 321325 * 424521 = ", 321325 * 424521);
+	// 연습문제 3
+	fmt.Println(len("日本語l"));
+	// 연습문제 5
+	fmt.Println((true && true)||(false && true)||!(false && false));
+
+}
