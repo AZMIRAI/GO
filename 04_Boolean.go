6cb8c9f33356c280283226445fcbfd277fbcf373
ファン・デヨン <77083563+KRHDY@users.noreply.github.com>
2021-04-23 19:43:02 +0900

    Add files via upload
    boolean

---
+++ 04_Boolean.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	// ******************************
+	// ********** 불 린 *************
+	// ******************************
+
+	fmt.Println(true && true);
+	// 참이고 참이면 참
+	fmt.Println(true && false);
+	// 참이고 거짓이면 거짓
+	fmt.Println(true || true);
+	// 참이거나 참이면 참
+	fmt.Println(true || false);
+	// 참이거나 거짓이면 참
+	fmt.Println(!true);
+	// 참의 거짓이면 거짓
+	fmt.Println(!!false);
+	// 거짓의 거짓의 거짓 거짓
+
+	;}
