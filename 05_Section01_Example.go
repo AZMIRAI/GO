0675dacdfb4375516f90e0493de27cca93c3d8ad
ファン・デヨン <77083563+KRHDY@users.noreply.github.com>
2021-04-23 20:20:13 +0900

    Update 05_Section01_Example.go

--- 05_Section01_Example.go
+++ 05_Section01_Example.go
@@ -10,8 +10,11 @@ func main()  {
 
 	fmt.Println(" 321325 * 424521 = ", 321325 * 424521);
 		// 연습문제 3
+
 	fmt.Println(len("日本語l"));
 		// 연습문제 4
+
 	fmt.Println((true && true)||(false && true)||!(false && false));
 		// 연습문제 5
+
 }
