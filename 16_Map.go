9288b3692c26a1d838024963be12b40ff10dc9bd
ファン・デヨン <77083563+KRHDY@users.noreply.github.com>
2021-05-07 20:39:06 +0900

    Update 16_Map.go
    アップデート

--- 16_Map.go
+++ 16_Map.go
@@ -4,17 +4,28 @@ import (
 	"fmt"
 )
 
+
+var input string;
+	// 変数inputをstring資料型で宣言する
+
 func main()  {
 
 	// ******************************
 	// *********** マップ ***********
 	// ******************************
 
+	fmt.Scanf("%s", &input);
+		// %sの位置に変数inputの値が入る。
+
 	map_01()
 		// map_01を呼び出す
 
 	map_02()
-		// map_01を呼び出す
+		// map_02を呼び出す
+
+	map_03()
+		// map_03を呼び出す
+
 }
 
 func map_01()  {
@@ -39,12 +50,106 @@ func map_02()  {
 	elements["He"] = "Helium"
 		// Heを押したらHeliumを出す
 
-	var input string;
-		// 変数inputをstring資料型で宣言する
+	elements["Li"] = "Lithium"
+		// Liを押したらLithiumを出す
 
-	fmt.Scanf("%s", &input);
-		// %sの位置に変数inputの値が入る。
+	elements["Be"] = "Beryllium"
+		// Beを押したらBerylliumを出す
+
+	elements["B"] = "Boron"
+		// Bを押したらBoronを出す
+
+	elements["C"] = "Carbon"
+		// Cを押したらCarbonを出す
+
+	elements["He"] = "Helium"
+		// Heを押したらHeliumを出す
+
+	elements["F"] = "Fluorine"
+		// Fを押したらFluorineを出す
+
+	elements["Ne"] = "Neon"
+		// Nを押したらNeonを出す
+
 
 	fmt.Println(elements[input])
-		// elementの”input”を出す
+	// elementの”input”を出す
+
+	fmt.Println(elements["un"])
+		// 多分、これしたら何も出てこないと思う
+
+	name, ok := elements["H"]
+		// name、okというelementsを宣言してHを登録する
+
+	fmt.Println(name, ok)
+		// name、okを出力する
+
+	elements_1 := map[string]string{
+		"H": "Hydrogen",
+		"He": "Helium",
+		"Li": "Lithium",
+		"Be": "Beryllium",
+		"B": "Boron",
+		"C": "Carbon",
+		"N": "Nitrogen",
+		"O": "Oxygen",
+		"F": "Fluorine",
+		"Ne": "Neon",
+	}
+		// 上のコードをもっと簡単に作れる
+
+	fmt.Println(elements_1)
+		// elements_1を出力する
+		
 }
+
+func map_03()  {
+	// このfunctionは普通の状態をセーブできるように作ったコードだ
+
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name":"Hydrogen",
+			"state":"gas",
+		},
+		"He": map[string]string{
+			"name":"Helium",
+			"state":"gas",
+		},
+		"Li": map[string]string{
+			"name":"Lithium",
+			"state":"solid",
+		},
+		"Be": map[string]string{
+			"name":"Beryllium",
+			"state":"solid",
+		},
+		"B": map[string]string{
+			"name":"Boron",
+			"state":"solid",
+		},
+		"C": map[string]string{
+			"name":"Carbon",
+			"state":"solid",
+		},
+		"N": map[string]string{
+			"name":"Nitrogen",
+			"state":"gas",
+		},
+		"O": map[string]string{
+			"name":"Oxygen",
+			"state":"gas",
+		},
+		"F": map[string]string{
+			"name":"Fluorine",
+			"state":"gas",
+		},
+		"Ne": map[string]string{
+			"name":"Neon",
+			"state":"gas",
+		},
+	}
+	if el, ok := elements[input]; ok {
+		fmt.Println(el["name"], el["state"])
+	}
+}
+
